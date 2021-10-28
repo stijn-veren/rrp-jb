a2465a1373e22d6db1d31689ed475d4ee79ddc3c
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-29 02:21:24 +0300

    Add css rule by const and choose the color by condition

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,5 +1,7 @@
 const TodoListItem = ({ label, important = false }) => {
-  return <span>{label}</span>
+  const style = { color: important ? 'tomato' : 'black' }
+
+  return <span style={style}>{label}</span>
 }
 
 export default TodoListItem
