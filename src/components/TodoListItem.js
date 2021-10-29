57d50d76eb855d23d100a878c812a73f50a10f9f
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-29 18:25:14 +0300

    Add buttons

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,12 +2,29 @@ import './TodoListItem.css'
 
 const TodoListItem = ({ label, important = false }) => {
   const style = {
-    color: important ? 'tomato' : 'black',
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal',
   }
 
   return (
-    <span className="todo-list-item" style={style}>
-      {label}
+    <span className="todo-list-item">
+      <span className="todo-list-item-label" style={style}>
+        {label}
+      </span>
+
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+      >
+        <i className="fas fa-exclamation" />
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+      >
+        <i className="far fa-trash-alt" />
+      </button>
     </span>
   )
 }
