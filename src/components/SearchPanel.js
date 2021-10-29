45c62694fce6c16859fc4c348b3d3ab0df97a17b
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-29 18:25:57 +0300

    Minor refactor and add styles

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,5 +1,13 @@
+import './SearchPanel.css'
+
 const SearchPanel = () => {
-  return <input placeholder="search" />
+  return (
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="type to search"
+    />
+  )
 }
 
 export default SearchPanel
