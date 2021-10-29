dda7b9261b5f27f7fcb307718ec4d86cfb4be0d2
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-29 17:53:32 +0300

    Remove App.js and move the code into index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import ReactDOM from 'react-dom'
-import App from './components/App'
+
+import AppHeader from './components/AppHeader'
+import SearchPanel from './components/SearchPanel'
+import TodoList from './components/TodoList'
+
+const App = () => {
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 },
+  ]
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={todoData} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
