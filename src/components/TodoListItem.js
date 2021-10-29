68dfc1de8237fa61da7313c63e3c8da6780808bc
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-29 03:01:26 +0300

    Add css to todo list item

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,9 +1,15 @@
+import './TodoListItem.css'
+
 const TodoListItem = ({ label, important = false }) => {
   const style = {
     color: important ? 'tomato' : 'black',
   }
 
-  return <span style={style}>{label}</span>
+  return (
+    <span className="todo-list-item" style={style}>
+      {label}
+    </span>
+  )
 }
 
 export default TodoListItem
