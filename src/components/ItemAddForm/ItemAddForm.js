9c0252f2312439425dc0c9eb9aa4b7cb2dda725d
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-30 01:22:39 +0300

    Add form to add new item

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -4,15 +4,26 @@ import './ItemAddForm.css'
 
 export default class ItemAddForm extends Component {
   render() {
+    const placeholderText = 'What needs to be done'
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex">
+        <input
+          type="text"
+          className="form-control mr-1"
+          placeholder={placeholderText}
+          aria-label={placeholderText}
+          aria-describedby="item-add-button"
+          onChange={this.onLabelChange}
+        />
         <button
           className="btn btn-outline-secondary"
+          type="button"
+          id="item-add-button"
           onClick={() => this.props.onItemAdded('Hello World')}
         >
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -4,7 +4,7 @@ const SearchPanel = () => {
   return (
     <input
       type="text"
-      className="form-control search-input"
+      className="form-control search-input mr-1"
       placeholder="type to search"
     />
   )
