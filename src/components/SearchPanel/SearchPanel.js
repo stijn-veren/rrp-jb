9994f211613543ff173152660016ce6bcc360894
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-30 02:00:24 +0300

    Add search functionality

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,13 +1,27 @@
+import { Component } from 'react'
+
 import './SearchPanel.css'
 
-const SearchPanel = () => {
-  return (
-    <input
-      type="text"
-      className="form-control search-input mr-1"
-      placeholder="type to search"
-    />
-  )
-}
+export default class SearchPanel extends Component {
+  state = {
+    term: '',
+  }
 
-export default SearchPanel
+  onSearchChange = (e) => {
+    const term = e.target.value
+    this.setState({ term })
+    this.props.onSearchChange(term)
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input mr-1"
+        placeholder="type to search"
+        value={this.state.term}
+        onChange={this.onSearchChange}
+      />
+    )
+  }
+}
