7577aa83d04efe3f1536fca4532c91291b60e320
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-29 21:20:25 +0300

    Refactor App component to class and add state to it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { Component } from 'react'
+
 import AppHeader from '../AppHeader'
 import SearchPanel from '../SearchPanel'
 import TodoList from '../TodoList'
@@ -5,27 +7,30 @@ import ItemStatusFilter from '../ItemStatusFilter'
 
 import './App.css'
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ]
+export default class App extends Component {
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={(id) => console.log(`Deleted ${id}`)}
+        />
       </div>
-
-      <TodoList
-        todos={todoData}
-        onDeleted={(id) => console.log(`Deleted ${id}`)}
-      />
-    </div>
-  )
+    )
+  }
 }
-
-export default App
