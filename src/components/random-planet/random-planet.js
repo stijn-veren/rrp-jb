366426f1459bca0dadb9604e866c16329e4c2686
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-31 01:02:05 +0300

    Remove extra files and refactor random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 
 import SwapiService from '../../services/swapi-service'
 import ErrorIndicator from '../error-indicator'
