07d60cbd3fb21db918f363b078c0d6019994b02c
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-30 19:18:02 +0300

    Add state and update planets for random planet component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,22 +1,54 @@
 import { Component } from 'react'
 
+import SwapiService from '../../services/swapi-service'
+
 import './random-planet.css'
 
 export default class RandomPlanet extends Component {
+  state = {
+    id: null,
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diameter: null,
+  }
+
+  constructor() {
+    super()
+    this.updatePlanet()
+  }
+
+  swapiService = new SwapiService()
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 60)
+    this.swapiService.getPlanet(id).then((planet) => {
+      this.setState({
+        id,
+        name: planet.name,
+        population: planet.population,
+        rotationPeriod: planet.rotation_period,
+        diameter: planet.diameter,
+      })
+    })
+  }
+
   render() {
+    const { id, name, population, rotationPeriod, diameter } = this.state
+
     return (
       <div className="row">
         <div className="col-4">
           <img
             alt=""
             className="img-fluid"
-            src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
+            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
           />
         </div>
         <div className="col-8 ps-3">
           <div className="row">
             <div className="col">
-              <h4>Planet Name</h4>
+              <h4>{name}</h4>
             </div>
           </div>
           <div className="row">
@@ -28,7 +60,7 @@ export default class RandomPlanet extends Component {
                       <span className="term">Population</span>
                     </div>
                     <div className="col col-md-7">
-                      <span>123124</span>
+                      <span>{population}</span>
                     </div>
                   </div>
                 </li>
@@ -38,7 +70,7 @@ export default class RandomPlanet extends Component {
                       <span className="term">Rotation Period</span>
                     </div>
                     <div className="col col-md-7">
-                      <span>43</span>
+                      <span>{rotationPeriod}</span>
                     </div>
                   </div>
                 </li>
@@ -48,7 +80,7 @@ export default class RandomPlanet extends Component {
                       <span className="term">Diameter</span>
                     </div>
                     <div className="col col-md-7">
-                      <span>100</span>
+                      <span>{diameter}</span>
                     </div>
                   </div>
                 </li>
