b333d6757afb6dda2df8af6bc24c2f98d172275a
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-01 00:39:42 +0200

    Refactor components

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 
 import ItemList from '../item-list'
-import PersonDetails from '../person-details'
-import ErrorBoundry from '../error-boundry'
+import ItemDetails from '../item-details'
+import SwapiService from '../../services/swapi-service'
 import Row from '../row'
+import ErrorBoundry from '../error-boundry'
 
 import './people-page.css'
-import SwapiService from '../../services/swapi-service'
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService()
 
   state = {
-    selectedPerson: 3,
+    selectedPerson: 11,
   }
 
   onPersonSelected = (selectedPerson) => {
@@ -21,19 +21,17 @@ export default class PeoplePage extends Component {
 
   render() {
     const itemList = (
-      <ErrorBoundry>
-        <ItemList
-          onItemSelected={this.onPersonSelected}
-          getData={this.swapiService.getAllPeople}
-        >
-          {(i) => `${i.name} (${i.birthYear})`}
-        </ItemList>
-      </ErrorBoundry>
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+      >
+        {(i) => `${i.name} (${i.birthYear})`}
+      </ItemList>
     )
 
     const personDetails = (
       <ErrorBoundry>
-        <PersonDetails personId={this.state.selectedPerson} />
+        <ItemDetails itemId={this.state.selectedPerson} />
       </ErrorBoundry>
     )
 
