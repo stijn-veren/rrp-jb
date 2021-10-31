18a085bed22cd386bacd6c38e47adbd175683b37
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-31 10:41:46 +0200

    Add property-component

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,6 +3,7 @@ import { Component } from 'react'
 import ItemList from '../item-list'
 import PersonDetails from '../person-details'
 import ErrorIndicator from '../error-indicator'
+import Row from '../row'
 
 import './people-page.css'
 import SwapiService from '../../services/swapi-service'
@@ -30,21 +31,18 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />
     }
 
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({ name, gender, birthYear }) =>
-              `${name} (${gender}, ${birthYear})`
-            }
-          />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name, gender, birthYear }) =>
+          `${name} (${gender}, ${birthYear})`
+        }
+      />
     )
+
+    const personDetails = <PersonDetails personId={this.state.selectedPerson} />
+
+    return <Row left={itemList} right={personDetails} />
   }
 }
