59034b9356930ae1a7b077c957e35ee877fa4820
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-31 10:22:55 +0200

    Add render functions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -60,6 +60,7 @@ export default class App extends Component {
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name}
             />
           </div>
           <div className="col-md-6">
@@ -72,6 +73,11 @@ export default class App extends Component {
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllStarships}
+              renderItem={(item) => (
+                <span>
+                  {item.name} <button>!</button>
+                </span>
+              )}
             />
           </div>
           <div className="col-md-6">
