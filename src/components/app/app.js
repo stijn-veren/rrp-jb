167330dc969303dbcfbdaf8dd87eba08bbdc6706
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-02 00:34:23 +0200

    Add dummy data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,7 @@ import ErrorBoundry from '../error-boundry'
 
 import ItemDetails, { Record } from '../item-details'
 import SwapiService from '../../services/swapi-service'
+import DummySwapiService from '../../services/dummy-swapi-service'
 
 import { SwapiServiceProvider } from '../swapi-service-context'
 
@@ -21,7 +22,8 @@ import {
 import './app.css'
 
 export default class App extends Component {
-  swapiService = new SwapiService()
+  swapiService = new DummySwapiService()
+  // swapiService = new SwapiService()
 
   state = {
     showRandomPlanet: true,
