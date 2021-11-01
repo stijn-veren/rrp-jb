345d1ee316c69456676b1c4db040b6bd5a397578
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-01 23:57:20 +0200

    Refactoring with higher order component composition

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -6,8 +6,27 @@ const swapiService = new SwapiService()
 
 const { getAllPeople, getAllStarships, getAllPlanets } = swapiService
 
-const PersonList = withData(ItemList, getAllPeople)
-const PlanetList = withData(ItemList, getAllPlanets)
-const StarshipList = withData(ItemList, getAllStarships)
+const withChildFunction = (Wrapped, fn) => {
+  return (props) => {
+    return <Wrapped {...props}>{fn}</Wrapped>
+  }
+}
+
+const renderName = ({ name }) => <span>{name}</span>
+
+const PersonList = withData(
+  withChildFunction(ItemList, renderName),
+  getAllPeople
+)
+
+const PlanetList = withData(
+  withChildFunction(ItemList, renderName),
+  getAllPlanets
+)
+
+const StarshipList = withData(
+  withChildFunction(ItemList, renderName),
+  getAllStarships
+)
 
 export { PersonList, PlanetList, StarshipList }
