4ce499de6abac092ce19781a9e9f28112ef95415
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-01 22:17:18 +0200

    Add HOC helper With data

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,49 +1,28 @@
-import { Component } from 'react'
-
+import { withData } from '../hoc-helpers'
+import SwapiService from '../../services/swapi-service'
 import './item-list.css'
-import Spinner from '../spinner'
 
-export default class ItemList extends Component {
-  state = {
-    itemList: null,
-  }
+const ItemList = (props) => {
+  const { data, onItemSelected, children: renderLabel } = props
 
-  componentDidMount() {
-    const { getData } = this.props
+  const items = data.map((item) => {
+    const { id } = item
+    const label = renderLabel(item)
 
-    getData().then((itemList) => {
-      this.setState({
-        itemList,
-      })
-    })
-  }
+    return (
+      <li
+        className="list-group-item"
+        key={id}
+        onClick={() => onItemSelected(id)}
+      >
+        {label}
+      </li>
+    )
+  })
 
-  renderItems(arr) {
-    return arr.map((item) => {
-      const { id } = item
-      const label = this.props.children(item)
-
-      return (
-        <li
-          className="list-group-item"
-          key={id}
-          onClick={() => this.props.onItemSelected(id)}
-        >
-          {label}
-        </li>
-      )
-    })
-  }
-
-  render() {
-    const { itemList } = this.state
-
-    if (!itemList) {
-      return <Spinner />
-    }
-
-    const items = this.renderItems(itemList)
-
-    return <ul className="item-list list-group">{items}</ul>
-  }
+  return <ul className="item-list list-group">{items}</ul>
 }
+
+const { getAllPeople } = new SwapiService()
+
+export default withData(ItemList, getAllPeople)
