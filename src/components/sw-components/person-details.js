fdb3809bc1fa93899ee7ae4e4b36d3a5e19b239d
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-02 00:58:04 +0200

    Transform Props in HOC p.2

--- src/components/sw-components/person-details.js
+++ src/components/sw-components/person-details.js
@@ -1,12 +1,9 @@
 import ItemDetails, { Record } from '../item-details'
 import { withSwapiService } from '../hoc-helpers'
 
-const PersonDetails = ({ itemId, getData, getImageUrl }) => {
+const PersonDetails = (props) => {
   return (
-    <ItemDetails
-      itemId={itemId}
-      getData={getData}
-      getImageUrl={getImageUrl}
+    <ItemDetails {...props}
     >
       <Record field="gender" label="Gender" />
       <Record field="eyeColor" label="Eye Color" />
