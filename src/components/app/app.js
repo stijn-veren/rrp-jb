5da861b4119585ee3d998ff88e324e244ca70479
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-01 22:16:18 +0200

    Add Item list to the App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,9 +4,11 @@ import Header from '../header'
 import RandomPlanet from '../random-planet'
 import ErrorBoundry from '../error-boundry'
 
-import Row from '../row/row'
+import Row from '../row'
 import ItemDetails, { Record } from '../item-details'
 import SwapiService from '../../services/swapi-service'
+
+import ItemList from '../item-list'
 
 import './app.css'
 
@@ -28,8 +30,14 @@ export default class App extends Component {
   render() {
     const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null
 
-    const { getPerson, getStarship, getPersonImage, getStarshipImage } =
-      this.swapiService
+    const {
+      getPerson,
+      getStarship,
+      getPersonImage,
+      getStarshipImage,
+      getAllPeople,
+      getAllPlanets,
+    } = this.swapiService
 
     const personDetails = (
       <ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage}>
@@ -55,7 +63,13 @@ export default class App extends Component {
         <div className="stardb-app">
           <Header />
 
-          <Row left={personDetails} right={starshipDetails} />
+          <ItemList getData={getAllPeople} onItemSelected={() => {}}>
+            {({ name }) => <span>{name}</span>}
+          </ItemList>
+
+          <ItemList getData={getAllPlanets} onItemSelected={() => {}}>
+            {({ name }) => <span>{name}</span>}
+          </ItemList>
         </div>
       </ErrorBoundry>
     )
