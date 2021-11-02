e34b3bbb04dc98f0ac596edb5d6129a076239f11
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-02 17:20:04 +0200

    Refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,24 +5,14 @@ import RandomPlanet from '../random-planet'
 import ErrorBoundry from '../error-boundry'
 import SwapiService from '../../services/swapi-service'
 import DummySwapiService from '../../services/dummy-swapi-service'
-import Row from '../row'
 
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages'
 import { SwapiServiceProvider } from '../swapi-service-context'
-
-import {
-  PersonDetails,
-  PlanetDetails,
-  StarshipDetails,
-  PersonList,
-  PlanetList,
-  StarshipList,
-} from '../sw-components'
 
 import './app.css'
 
 export default class App extends Component {
   state = {
-    showRandomPlanet: true,
     swapiService: new DummySwapiService(),
   }
 
@@ -37,33 +27,17 @@ export default class App extends Component {
     })
   }
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet,
-      }
-    })
-  }
-
   render() {
-    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null
-
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService}>
           <div className="stardb-app">
             <Header onServiceChange={this.onServiceChange} />
 
-            {planet}
-
-            <Row left={<PersonList />} right={<PersonDetails itemId={11} />} />
-
-            <Row left={<PlanetList />} right={<PlanetDetails itemId={5} />} />
-
-            <Row
-              left={<StarshipList />}
-              right={<StarshipDetails itemId={9} />}
-            />
+            <RandomPlanet />
+            <PeoplePage />
+            <PlanetsPage />
+            <StarshipsPage />
           </div>
         </SwapiServiceProvider>
       </ErrorBoundry>
--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,11 +1,13 @@
 import { Component } from 'react'
-
 import Spinner from '../spinner'
+import ErrorIndicator from '../error-indicator'
 
 const withData = (View) => {
   return class extends Component {
     state = {
       data: null,
+      loading: true,
+      error: false,
     }
 
     componentDidUpdate(prevProps) {
@@ -19,18 +21,36 @@ const withData = (View) => {
     }
 
     update() {
-      this.props.getData().then((data) => {
-        this.setState({
-          data,
-        })
+      this.setState({
+        loading: true,
+        error: false,
       })
+
+      this.props
+        .getData()
+        .then((data) => {
+          this.setState({
+            data,
+            loading: false,
+          })
+        })
+        .catch(() => {
+          this.setState({
+            error: true,
+            loading: false,
+          })
+        })
     }
 
     render() {
-      const { data } = this.state
+      const { data, loading, error } = this.state
 
-      if (!data) {
+      if (loading) {
         return <Spinner />
+      }
+
+      if (error) {
+        return <ErrorIndicator />
       }
 
       return <View {...this.props} data={data} />
