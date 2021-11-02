013bb329ea66ee598e2cb5621a9b9cf8856f08b9
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-02 20:04:29 +0200

    Add useState

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-import ReactDOM from "react-dom";
+import { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => <p>Hi</p>;
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const HookSwitcher = () => {
+  const [color, setColor] = useState('white')
+  const [fontSize, setFontSize] = useState(24)
+
+  const fontColor = color === 'black' ? 'white' : 'black'
+
+  return (
+    <div
+      style={{
+        padding: '10px',
+        color: fontColor,
+        backgroundColor: color,
+        fontSize: `${fontSize}px`,
+      }}
+    >
+      <p>Hello, World!</p>
+      <button onClick={() => setColor('black')}>Dark</button>
+      <button onClick={() => setColor('white')}>Light</button>
+      <button onClick={() => setFontSize((s) => s + 2)}>+</button>
+      <button onClick={() => setFontSize((s) => s - 2)}>-</button>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
