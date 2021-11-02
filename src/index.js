3febd34857c6e9a47601ac6ab3853e26979379af
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-03 01:58:49 +0200

    Add useEffect

--- src/index.js
+++ src/index.js
@@ -1,20 +1,67 @@
-import React, { useContext } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-const MyContext = React.createContext()
-
 const App = () => {
-  return (
-    <MyContext.Provider value="Hello, World!">
-      <Child />
-    </MyContext.Provider>
-  )
+  const [value, setValue] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  if (visible) {
+    return (
+      <div>
+        <button onClick={() => setValue((v) => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <ClassCounter value={value} />
+        <HookCounter value={value} />
+      </div>
+    )
+  } else {
+    return <button onClick={() => setVisible(true)}>show</button>
+  }
 }
 
-const Child = () => {
-  const value = useContext(MyContext)
+const HookCounter = ({ value }) => {
+  // componentDidMount, componentDidUpdate
+  useEffect(() => {
+    console.log('useEffect as componentDidMount and componentDidUpdate')
+  })
+
+  // componentDidMount
+  useEffect(() => {
+    console.log('useEffect as componentDidMount only')
+  }, [])
+
+  // componentDidMount, componentDidUpdate
+  useEffect(() => {
+    console.log('useEffect as componentDidMount and componentDidUpdate')
+  }, [value])
+
+  // componentDidMount, componentDidUpdate, componentWillUnmount
+  useEffect(() => {
+    console.log(
+      'useEffect as componentDidMount, componentDidUpdate and componentWillUnmount'
+    )
+    return () => console.log('clear')
+  }, [value])
 
   return <p>{value}</p>
+}
+
+class ClassCounter extends Component {
+  componentDidMount() {
+    console.log('class: mount')
+  }
+
+  componentDidUpdate(props) {
+    console.log('class: update')
+  }
+
+  componentWillUnmount() {
+    console.log('class: unmount')
+  }
+
+  render() {
+    return <p>{this.props.value}</p>
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
