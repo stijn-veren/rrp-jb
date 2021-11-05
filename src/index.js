79d4337db09365043500a25b7e5f5aafb2243a67
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-06 01:27:45 +0200

    Add an example of base reducer logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+const reducer = (state, action) => {
+  if (state === undefined) {
+    return 0
+  }
+
+  switch (action.type) {
+    case 'INC':
+      return state + 1
+
+    default:
+      return state
+  }
+}
+
+let state = reducer(undefined, {})
+
+state = reducer(state, { type: 'INC' })
+
+console.log(state)
