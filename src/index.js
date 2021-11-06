86cffb1b66ceeded9f27a9f84de474fadf48cc0e
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-06 02:38:29 +0200

    Add random through payload

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ const reducer = (state, action) => {
     case 'DEC':
       return state - 1
 
+    case 'RND':
+      return state + action.payload
+
     default:
       return state
   }
@@ -25,6 +28,11 @@ document.getElementById('inc').addEventListener('click', () => {
 
 document.getElementById('dec').addEventListener('click', () => {
   store.dispatch({ type: 'DEC' })
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const payload = Math.floor(Math.random() * 9)
+  store.dispatch({ type: 'RND', payload })
 })
 
 const update = () => {
