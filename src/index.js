70ca23614bcb07f1281705be6c890562d6e72036
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-06 02:52:47 +0200

    Simplify to wrap the action creator in dispatch

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import reducer from './reducer'
 import { inc, dec, rnd } from './actions'
 
 const store = createStore(reducer)
+const { dispatch } = store
 
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc())
-})
+const incDispatch = () => dispatch(inc())
+const decDispatch = () => dispatch(dec())
+const rndDispatch = (payload) => dispatch(rnd(payload))
 
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec())
-})
-
+document.getElementById('inc').addEventListener('click', incDispatch)
+document.getElementById('dec').addEventListener('click', decDispatch)
 document.getElementById('rnd').addEventListener('click', () => {
   const payload = Math.floor(Math.random() * 9)
-  store.dispatch(rnd(payload))
+  rndDispatch(payload)
 })
 
 const update = () => {
