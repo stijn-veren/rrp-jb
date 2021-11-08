aa7f09f482753bd4819495866982734b854c35bf
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-08 20:58:22 +0200

    Add error catch

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -19,9 +19,10 @@ export default class BookstoreService {
   ]
 
   getBooks() {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(this.data)
+        // resolve(this.data)
+        reject(new Error('Something bad happened'))
       }, 700)
     })
   }
