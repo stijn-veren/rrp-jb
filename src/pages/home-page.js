b4e4849c55df48248c83ac15846cee4b9fb73899
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-08 15:38:52 +0200

    Make home page show book list

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,5 +1,19 @@
+import BookList from '../book-list'
+
 const HomePage = () => {
-  return <div>Home Page</div>
+  const books = [
+    {
+      id: 1,
+      title: 'Production-Ready Microservices',
+      author: 'Susan J. Fowler',
+    },
+    {
+      id: 2,
+      title: 'Release It!',
+      author: 'Michael T. Nygard',
+    },
+  ]
+  return <BookList books={books} />
 }
 
 export default HomePage
