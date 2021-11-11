83ac343328b7ffe86aa4938eb71e469bbc1a9b63
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-11 17:28:37 +0200

    Create separate BookListContainer

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,21 @@ import ErrorIndicator from '../error-indicator'
 
 import './book-list.css'
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {books.map((book) => {
+        return (
+          <li key={book.id}>
+            <BookListItem book={book} />
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+class BookListContainer extends Component {
   componentDidMount() {
     this.props.fetchBooks()
   }
@@ -26,17 +40,7 @@ class BookList extends Component {
       return <ErrorIndicator />
     }
 
-    return (
-      <ul className="book-list">
-        {books.map((book) => {
-          return (
-            <li key={book.id}>
-              <BookListItem book={book} />
-            </li>
-          )
-        })}
-      </ul>
-    )
+    return <BookList books={books} />
   }
 }
 
@@ -53,4 +57,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList)
+)(BookListContainer)
