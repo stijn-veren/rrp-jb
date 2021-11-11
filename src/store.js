4af19d9b72bc33dbbd21b6eebbd28db26847b649
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-12 00:05:35 +0200

    Add Thunk

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 
 import reducer from './reducers'
 
@@ -22,9 +23,19 @@ const stringMiddleware = () => (next) => (action) => {
 
 const store = createStore(
   reducer,
-  applyMiddleware(stringMiddleware, logMiddleware)
+  applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware)
 )
 
-store.dispatch('HELLO_WORLD')
+const delayedActionCreator = (timeout) => (dispatch) => {
+  setTimeout(
+    () =>
+      dispatch({
+        type: 'DELAYED_ACTION',
+      }),
+    timeout
+  )
+}
+
+store.dispatch(delayedActionCreator(3000))
 
 export default store
