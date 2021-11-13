1facd7c3af8fad0a7f9bb715d50e9bcfb533f02f
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-13 22:31:03 +0200

    build-sandbox: Check how to switch to browser versions

--- build-sandbox/build/main.js
+++ build-sandbox/build/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -7,11 +11,28 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 var App = function App() {
   _classCallCheck(this, App);
 
-  _defineProperty(this, "run", function () {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'World';
-    console.log("Hello ".concat(name));
-  });
+  _defineProperty(this, "run", /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var name,
+        _args = arguments;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            name = _args.length > 0 && _args[0] !== undefined ? _args[0] : 'World';
+            console.log("Hello ".concat(name));
+
+          case 2:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  })));
 };
 
 var app = new App();
-app.run();
+app.run().then(function () {
+  return console.log('done');
+}).catch(function () {
+  return console.log('Error!');
+});
