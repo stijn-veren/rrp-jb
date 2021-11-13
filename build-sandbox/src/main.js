ede86f7aa95b4bc8451ec583f1ab469e79d33ec0
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-13 04:09:35 +0200

    build-sandbox: Use babel

--- build-sandbox/src/main.js
+++ build-sandbox/src/main.js
@@ -7,3 +7,5 @@ class App {
 
 const app = new App()
 app.run()
+
+// npx babel src --out-dir build --plugins @babel/plugin-transform-template-literals,@babel/plugin-transform-block-scoping,@babel/plugin-transform-classes
